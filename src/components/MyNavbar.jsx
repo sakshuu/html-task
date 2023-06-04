c393e32a8fb8638dbaf20295a0f6347bddf7eea5
sakshi jadhav <sakshisjadhav.120@gmail.com>
2023-06-04 11:07:40 +0530

    started project

---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MyNavbar = () => {
+  return (
+    <div>MyNavbar</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// import 'tailwindcss/base';
+// import 'tailwindcss/components';
+// import 'tailwindcss/utilities';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
